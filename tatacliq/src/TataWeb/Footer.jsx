ef6e876dc14b1a3479a858e31e520a8181322fae
pooh-14 <poojavetal14@gmail.com>
2023-07-02 16:16:56 +0530

    done

--- tatacliq/src/TataWeb/Footer.jsx
+++ tatacliq/src/TataWeb/Footer.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div id="footer">
+      <div>
+        <img src="https://www.tatacliq.com/src/general/components/img/trustFrame.png"/>
+        <img src="https://www.tatacliq.com/src/general/components/img/Frame22222.svg"/>
+      </div>
+      <div>
+       <div>
+        <h4>Tata MarketPlace</h4>
+        <p> About Us</p>
+        <p>Careers</p>
+        <p>Sell With Us</p>
+        <p>Terms of Use</p>
+        <p>Privacy Policy</p>
+        <p>Affiliates</p>
+        <p>Sitemap</p>
+       </div>
+       <div>
+        <h4>Customer Service</h4>
+       <p>Shopping</p>
+       <p>Offers & Promotions</p>
+       <p>Payments</p>
+       <p>Cancellation</p>
+       <p>Returns & Refunds</p>
+       <p>CliQ And PiQ</p>
+       <p>Return To Store</p>
+       <p>Electronics Return Policy</p>
+       <p>Contact Us</p>
+       <p>Reviews Guidelines</p>
+       <p>Furniture Return Policy</p>
+       <p>Replacement Policy</p>
+       </div>
+       <div>
+        <h4>My Tata CLiQ</h4>
+        <p>My Account</p>
+        <p>My Orders</p>
+        <p>My Shopping Bag</p>
+        <p>My Wishlist</p>
+       </div>
+       <div>
+        <h4>Tata CLiQ Offerings</h4>
+      <p>Watches for Men|Campus Shoes|Sandals for Men|Sneakers for Men|Reebok Shoes|Cotton Kurtis|Woodland Shoes|Jumpsuits|Allen Solly|Sparx Shoes|Gold Rings|Formal Shoes for Men|Sports Shoes for Men|Wallets for Men|Ladies Watches|Trolley Bags|Handbags for Women|Sling Bags for Women|Casual Shoes for Men
+  |Boots for Men|Digital Watches|Sonata Watches|Sneakers for Women
+  |Running Shoes|Puma Shoes|Boots for Women|Skechers|Malabargold
+          |Fabindia|Utsa|Vark|Gia|LOV|Sitemap</p>
+       </div>
+      </div>
+    </div>
   )
 }
 
--- tatacliq/src/TataWeb/Navbar.jsx
+++ tatacliq/src/TataWeb/Navbar.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import './Navbar.css'
 
+
+
 const Navbar = () => {
   return (
     <div id="navbar">
         <div id="left">
+          <img src="http://www.pngimagesfree.com/LOGO/T/Tata-CLiQ/Tata-cliq-logo-PNG-Black-and-White.png"/>
         </div>
         <div id="right">
-            <div id="up"></div>
-            <div id="down"></div>
+            <div id="up">
+              <p>Tata CLiQ Luxury</p>
+              <span>
+                <p>CLiQ Cash</p>
+                <p>Gift Card</p>
+                <p>CliQ Care</p>
+                <p>Track Orders</p>
+                <p>Sign in/ Sign up</p>
+              </span>
+            </div>
+            <div id="down">
+              <div>
+                <p>Categories</p>
+               <i className="fa-solid fa-angle-down" ></i>
+              </div>
+              <div>
+               <p>Brands</p>
+               <i className="fa-solid fa-angle-down" ></i>
+              </div>
+              <div>
+              <i class="fa-solid fa-magnifying-glass"></i>
+              <input  placeholder='Search for Products'/>
+              </div>
+              <div>
+              <i class="fa-regular fa-heart fa-xl"></i>
+              <i class="fa-solid fa-bag-shopping fa-xl"></i>
+              </div>
+            </div>
         </div>
     </div>
   )
