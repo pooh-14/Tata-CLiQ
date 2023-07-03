afac3bad828465f874c4ecb461d628ca3c9d3659
pooh-14 <poojavetal14@gmail.com>
2023-07-03 14:44:45 +0530

    done

--- tatacliq/src/TataWeb/Home.jsx
+++ tatacliq/src/TataWeb/Home.jsx
@@ -1,9 +1,195 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <div id="slide">
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855525406.jpg'/>
+      </div>
+      <div id="cate">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596848218142.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47548678373406.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466152990.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466218526.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47575793303582.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466349598.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466415134.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466480670.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466546206.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47445466611742.jpg'/>
+        </div>
+      </div>
+      <div id="pay">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/46762762436638.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855787550.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/46690782937118.jpg'/>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47532422234142.png'/>
+      </div>
+      <div id="block">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857851934.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865257502.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596857983006.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596866830366.jpg'/>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344211486.jpg'/>
+      </div>
+      <div id="deals">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg'/>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg'/>
+      </div>
+      <div id="circle">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47513577619486.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47513577685022.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47513577750558.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47513577816094.jpg'/>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344408094.jpg'/>
+      </div>
+      <div id="featured">
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596866895902.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596867223582.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596867944478.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596866961438.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596854804510.jpg'/>
+        </div>
+        <div>
+          <img src='https://assets.tatacliq.com/medias/sys_master/images/47596854870046.jpg'/>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344932382.jpg'/>
+      </div>
+      <div id="her">
+        <div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394824421406.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394824486942.jpg'/>
+          </div>
+        </div>
+        <div>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855197726.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47525840519198.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47532442583070.jpg'/>
+          </div>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47415375757342.jpg'/>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520344997918.jpg'/>
+      </div>
+      <div id="his">
+        <div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394802270238.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394802335774.jpg'/>
+          </div>
+        </div>
+        <div>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47596855001118.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394802466846.jpg'/>
+          </div>
+          <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394802663454.jpg'/>
+          </div>
+        <div>
+            <img src='https://assets.tatacliq.com/medias/sys_master/images/47394802532382.jpg'/>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520345948190.jpg'/>
+      </div>
+      <div id="woman">
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47385014632478.jpg'/>
+      </div>
+      <div>
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47520346013726.jpg'/>
+      </div>
+      <div id="man">
+        <img src='https://assets.tatacliq.com/medias/sys_master/images/47385014698014.jpg'/>
+      </div>
     </div>
   )
 }
