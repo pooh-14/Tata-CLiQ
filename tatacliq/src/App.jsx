05e0b7069ff50e771fd630b03bce70f3c871be7d
pooh-14 <poojavetal14@gmail.com>
2023-07-07 20:17:02 +0530

    done

--- tatacliq/src/App.jsx
+++ tatacliq/src/App.jsx
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route,Routes } from "react-router-dom";
-import Home from './TataWeb/Home';
-import Navbar from './TataWeb/Navbar';
-import Footer from './TataWeb/Footer';
-import MensSection from './TataWeb/MensSection';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./TataWeb/Home";
+import Navbar from "./TataWeb/Navbar";
+import Footer from "./TataWeb/Footer";
+import MensSection from "./TataWeb/MensSection";
+import Mensmultiple from "./../src/TataWeb/Mensmultiple";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
+    <div>
+      <Navbar />
       <Routes>
-        <Route exact path='/home' element={<Home/>}/>
-        <Route exact path='/menssection' element={<MensSection  />}/>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/menssection" element={<MensSection />} />
+        <Route exact path="/mensmultiple" element={<Mensmultiple/>}/>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ tatacliq/src/TataWeb/Mensmultiple.jsx
@@ -0,0 +1,114 @@
+import "./../TataWeb/Mensmultiple.css";
+function Mensmultiple() {
+  return(
+    <div id="menz">
+      <div id="one">
+        <p>Men's winter wear</p>
+        <p>110919 Products</p>
+      </div>
+      <div id="two">
+        <div>
+        <span>Sort by:</span>
+        <span>
+          <select>
+            <option>Popularity</option>
+            <option>Price High to Low</option>
+            <option>Price low to High</option>
+            <option>New Arrivals</option>
+            <option>Discounts</option>
+          </select>
+          <i class="fa-solid fa-arrow-down-wide-short"></i>
+        </span>
+        </div>
+        <span>
+          <img src="https://www.tatacliq.com/src/plp/components/img/list.svg" />
+        </span>
+      </div>
+      <div id="body">
+        <div id="mensleft">
+            <div>
+                <p>Filters</p>
+                <p>Clear All</p>
+            </div>
+            <div>
+                <p>Department</p>
+                <p>Mens Clothing</p>
+            </div>
+            <div>
+                <p>Category</p>
+                <span>
+                    <p>Casual Wear</p>
+                    <i class="fa-solid fa-xmark"></i>
+                </span>
+            </div>
+            <div>
+            <div>
+                <p>Product Type</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Brand</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Size</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Type</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Color</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Price</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Fit</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Discount</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Fabric Family</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Sleeve</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Collar</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Pattern</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Ocassion</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>All Discount</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div>
+            <p>Availability</p>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            </div>
+        </div>
+        <div id="mensright">
+            
+        </div>
+      </div>
+    </div>
+     )
+}
+export default Mensmultiple;
